0520c7ed4af89965a89c935ba5a556823f690a18
Peregrino Tabudlong Jr <peregrino.tabudong@gmail.com>
2017-06-27 11:13:03 +0800

    Initial migration - teacher model

--- focusappsystem/settings.py
+++ focusappsystem/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'school',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'focusappsystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'focusappsystem',
+        'USER': 'peregrino.t',
+        'PASSWORD': 'tabudlong2016',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
--- school/models.py
+++ school/models.py
@@ -2,4 +2,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Teacher(models.Model):
+	firstName = models.CharField(max_length=200)
+	lastName = models.CharField(max_length=200)
+	middleName = models.CharField(max_length=200)
+	emailAddress = models.EmailField()
