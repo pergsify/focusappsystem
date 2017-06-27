1b80188691a2ef4c037ddce15836f83bbd81ec75
Peregrino Tabudlong Jr <peregrino.tabudong@gmail.com>
2017-06-27 20:31:17 +0800

    Teacher lists

--- school/admin.py
+++ school/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import(Teacher)
+
+class TeacherAdmin(admin.ModelAdmin):
+	list_display = ['firstName', 'lastName']
+
+admin.site.register(Teacher, TeacherAdmin)
