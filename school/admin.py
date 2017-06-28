42857b5c35aa8c52c2b33635df9bc2128805e7d9
Peregrino Tabudlong Jr <peregrino.tabudong@gmail.com>
2017-06-28 18:32:43 +0800

    Feature - Teacher

--- school/admin.py
+++ school/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
-from .models import(Teacher)
+from .models import(Teacher, SchoolYear)
 
 class TeacherAdmin(admin.ModelAdmin):
-	list_display = ['firstName', 'lastName']
+	list_display = ['first_Name', 'last_Name']
+
+class SchoolYearAdmin(admin.ModelAdmin):
+	list_display = ['start_Year']
 
 admin.site.register(Teacher, TeacherAdmin)
+admin.site.register(SchoolYear, SchoolYearAdmin)
--- school/models.py
+++ school/models.py
@@ -2,8 +2,17 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+import datetime
+YEAR_CHOICES = []
+for r in range(1980, (datetime.datetime.now().year+1)):
+    YEAR_CHOICES.append((r,r))
+
 class Teacher(models.Model):
-	firstName = models.CharField(max_length=200)
-	lastName = models.CharField(max_length=200)
+	first_Name = models.CharField(max_length=200)
+	last_Name = models.CharField(max_length=200)
 	middleName = models.CharField(max_length=200)
 	emailAddress = models.EmailField()
+
+class SchoolYear(models.Model):
+	#start_Year = models.DateField()
+	start_Year = models.IntegerField(max_length=4, choices=YEAR_CHOICES, default=datetime.datetime.now().year)
